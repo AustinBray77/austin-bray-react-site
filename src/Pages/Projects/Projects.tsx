9ef8d012769fd0c9ab1a5b59397a77a6056a23e0
AustinBray77 <austin.bray77@gmail.com>
2022-06-24 18:04:27 -0400

    Updated Project Page

--- src/Pages/Projects/Projects.tsx
+++ src/Pages/Projects/Projects.tsx
@@ -32,7 +32,31 @@ export default function Projects(): JSX.Element {
 					title="Block Snake 2D"
 					img="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
 				>
-					<p>Block Snake 2D is my first major Unity project coded in C#</p>
+					<p>
+						Block Snake 2D is my first major Unity project coded in C#. The goal
+						of this project was to see through the full publication of an app on
+						a major distrubting platform (google play store) and to develop a
+						game for android through Unity, which I had not done before. I
+						believe I was successful in both of these goals and it really taught
+						me alot about how much it actaully takes to make a fully functioning
+						app and how to push it to production. To download and play Block
+						Snake 2D , click <a href="www.googleplaystore.com/">here</a>. To see
+						the source code for this project, go to the source code page.
+					</p>
+				</ProjRow>
+				<ProjRow
+					title="Conway's Game Of Life"
+					img="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+				>
+					<p>
+						A replication of conway's game of life was another one of my major
+						projects. I used to C++ and the WIN 32 Api for windows to create
+						this project. My goal from this project was to learn more about
+						object oriented C++ and the WIN 32 Api, which I believe I was
+						successful in doing. To use this project simply go to the downloads
+						page and download it from there, it is only available for windows
+						computers.
+					</p>
 				</ProjRow>
 			</Container>
 		</div>
