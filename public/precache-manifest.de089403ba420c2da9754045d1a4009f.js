4dd02cbbba4c3c730b265e68c77db1eb7836f9b5
deoignition <93416177+AustinBray77@users.noreply.github.com>
2023-04-03 17:40:52 -0400

    Public folder static changes

---
+++ public/precache-manifest.de089403ba420c2da9754045d1a4009f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "809c348e0df29ecc2050",
+    "url": "/static/css/main.3aa5c43f.chunk.css"
+  },
+  {
+    "revision": "809c348e0df29ecc2050",
+    "url": "/static/js/main.9a4fe341.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0cf0b29a8c9fa07c880a",
+    "url": "/static/css/2.5a223140.chunk.css"
+  },
+  {
+    "revision": "0cf0b29a8c9fa07c880a",
+    "url": "/static/js/2.ab0b8bf5.chunk.js"
+  },
+  {
+    "revision": "25373831120a56f7a87d51d556eff8e2",
+    "url": "/index.html"
+  }
+];
