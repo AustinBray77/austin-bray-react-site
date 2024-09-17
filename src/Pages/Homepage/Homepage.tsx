2bdd3592d38363a7d0bb24b4ae2badab44ff559b
deoignition <93416177+AustinBray77@users.noreply.github.com>
2024-09-16 20:08:52 -0400

    Changed full homepage to new format, need to change image position for last row

--- src/Pages/Homepage/Homepage.tsx
+++ src/Pages/Homepage/Homepage.tsx
@@ -60,130 +60,54 @@ const ProjectRow = () => {
 };
 
 const FreelanceRow = () => {
-    const FreelanceRef = useRef<HTMLDivElement>(null);
-    const [animateFreelanceRow, setAnimateFreelanceRow] = useState(false);
-
-    useEffect(() => {
-        const observer = new IntersectionObserver((entries) => {
-            let entry = entries[0];
-            if (entry.isIntersecting) {
-                setAnimateFreelanceRow(true);
-            }
-        });
-
-        if (FreelanceRef.current != null) {
-            observer.observe(FreelanceRef.current);
-        }
-    }, []);
-
     return (
-        <div ref={FreelanceRef} className="content-row">
-            <Row className="bg-pl text-pd p-5">
-                {animateFreelanceRow ? (
-                    <>
-                        {" "}
-                        <Col
-                            lg={6}
-                            xs={12}
-                            className="text-center text-lg-start"
-                        >
-                            <h1 className="fw-bold">
-                                <Typewriter speed={100}>
-                                    Your Website, Your Way
-                                </Typewriter>
-                            </h1>
-                            <br />
-                            <p className="fs-4 info-paragraph">
-                                <Typewriter speed={25}>
-                                    I have plenty of experience creating various
-                                    beautiful websites for my self and for
-                                    others. To learn more on what I can do for
-                                    you and the services I offer, visit my
-                                    freelancing page.
-                                </Typewriter>
-                            </p>
-                            <LinkButton path="/freelance">
-                                <Typewriter
-                                    children={"See what I can do for you ->"}
-                                    speed={100}
-                                />
-                            </LinkButton>
-                        </Col>
-                        <Col lg={6} xs={12} className="image-col">
-                            <Row className="justify-content-center fade-in">
-                                <div className="img-container">
-                                    <img src="./citl.png" />
-                                </div>
-                            </Row>
-                        </Col>{" "}
-                    </>
-                ) : (
-                    ""
-                )}
-            </Row>
-        </div>
+        <StandardRow
+            title="Your Website, Your way"
+            heightFunc={(size: WindowSizes): number => {
+                if (size == WindowSizes.XS) {
+                    return 25;
+                }
+                if (size <= WindowSizes.MD) {
+                    return 7.5;
+                }
+
+                return 10;
+            }}
+            body={{
+                children:
+                    "I have plenty of experience creating various beautiful websites for my self and for others. To learn more on what I can do for you and the services I offer, visit my freelancing page.",
+            }}
+            button="See what I can do for you ->"
+            path="/freelance"
+            img={{ src: "./citl.png" }}
+            ratio={6}
+        />
     );
 };
 
 const ExperienceRow = () => {
-    const ExperienceRef = useRef<HTMLDivElement>(null);
-    const [animateExperienceRow, setAnimateExperienceRow] = useState(false);
-
-    useEffect(() => {
-        const observer = new IntersectionObserver((entries) => {
-            let entry = entries[0];
-            if (entry.isIntersecting) {
-                setAnimateExperienceRow(true);
-            }
-        });
-
-        if (ExperienceRef.current != null) {
-            observer.observe(ExperienceRef.current);
-        }
-    }, []);
-
     return (
-        <div ref={ExperienceRef} className="content-row">
-            <Row className="bg-pl text-pd p-5">
-                {animateExperienceRow ? (
-                    <>
-                        <Col lg={6} xs={12} className="image-col">
-                            <Row className="fade-in justify-content-center">
-                                <div className="img-container">
-                                    <img src="./experience.png" />
-                                </div>
-                            </Row>
-                        </Col>
-                        <Col lg={6} xs={12} className="text-center text-lg-end">
-                            <h1 className="fw-bold">
-                                <Typewriter speed={100}>
-                                    Experience and Resume
-                                </Typewriter>
-                            </h1>
-                            <br />
-                            <p className="fs-4 info-paragraph">
-                                <Typewriter speed={25}>
-                                    I have experience working with C#, C++,
-                                    Java, Javascript, and Python, among other
-                                    languages. I also have experience working
-                                    with many different technologies involved
-                                    with this languages such as Unity, the MERN
-                                    stack, WIN32 etc. To get more about my
-                                    expierence and skills check out my resume.
-                                </Typewriter>
-                            </p>
-                            <LinkButton path="/about-me">
-                                <Typewriter speed={100}>
-                                    {"View more and my resume ->"}
-                                </Typewriter>
-                            </LinkButton>
-                        </Col>
-                    </>
-                ) : (
-                    ""
-                )}
-            </Row>
-        </div>
+        <StandardRow
+            title="Experience and Resume"
+            heightFunc={(size: WindowSizes): number => {
+                if (size == WindowSizes.XS) {
+                    return 25;
+                }
+                if (size <= WindowSizes.MD) {
+                    return 7.5;
+                }
+
+                return 10;
+            }}
+            body={{
+                children:
+                    "I have experience working with C#, C++, Java, Javascript, and Python, among other languages. I also have experience working with many different technologies involved with this languages such as Unity, the MERN stack, WIN32 etc. To get more about my expierence and skills check out my resume.",
+            }}
+            button="View more and my resume ->"
+            path="/about-me"
+            img={{ src: "./experience.png" }}
+            ratio={6}
+        />
     );
 };
 
